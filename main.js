6c477890d5d178d125c7f8951a0c5c606bf1dbd0
k0502s <[k0502s@naver.com]>
2021-01-09 23:48:22 +0900

    Node.js and myssql 공부
    글 생성 구현하기 추가 중

--- main.js
+++ main.js
@@ -97,9 +97,9 @@ var app = http.createServer(function(request,response){
           });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
+      db.query(`SELECT * FROM topic`, function(error, topics){
+        var title = 'Create';
+        var list = template.list(topics);
         var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
@@ -110,7 +110,8 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+        `,
+        `<a href="/create">create</a>`   );                      
         response.writeHead(200);
         response.end(html);
       });
@@ -121,13 +122,23 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
+          /*var title = post.title;
           var description = post.description;
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
-          })
-      });
+          })*/
+           db.query(`INSERT INTO topic (title, description, created, author_id) 
+              VALUES(?, ?, NOW(), ?)`,
+            [post.title, post.description, 1], function(error, result){
+               if(error){
+                   throw error;
+               }
+               response.writeHead(302, {Location: `/?id=${result.insertId}`});
+               response.end();
+               }
+                     )
+          });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
